cad41f9dacf8d30b1b8e4e25926bccc9e33a5f10
Mehdi Saffar <saffar.mehdi3@gmail.com>
2021-10-11 23:42:32 +0300

    track

--- _hooks/README.md
+++
@@ -1,1 +0,0 @@
-Make git use these hooks by executing `git config core.hooksPath _hooks` (requires Git 2.9)
--- _scripts/replace-in-file.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    files: ["**/*.md"],
-    ignore: ["node_modules/**/*.*", "_*/**/*.*"],
-    from: /\t/g,
-    to: "    "
-};
